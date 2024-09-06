fc7eb0174491af2c7df866fe81f0f7b87a7722b5
Jairo <jairomunier@gmail.com>
2024-09-06 19:58:04 -0300

    projeto-mecanismo-pesquisa

---
+++ projeto_pesquisa_animais/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "cachorrinhoanimado",
+        descricao: "Cachorro é um animal é um animal muito fofo e animado",
+        link: "https://love.doghero.com.br/caes/cachorros-fofos/",
+        tags: ["cachorro", "fofo", "animado"]
+    },
+    {
+        titulo: "gato",
+        descricao: "Gato é um animal é um animal muito fofo e docil",
+        link: "https://www.petlove.com.br/dicas/gatinhos-fofos-veja-fotos",
+        tags: ["gato", "fofo", "docil"]
+    },
+    {
+        titulo: "capivara",
+        descricao: "capivara é um animal silvestre e proibido de criação",
+        link: "https://www.petlove.com.br/dicas/gatinhos-fofos-veja-fotos",
+        tags: ["capivara", "silvestre", "proibido", "filó"]
+    }
+];
